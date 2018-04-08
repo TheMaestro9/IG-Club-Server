7953302bc29bb0d706ea22753efbe8c082a717a5
muha-abdulaziz <muha.abdulaziz@gmail.com>
2018-04-08 16:30:59 +0200

    Fix the user router.

--- routers/user.js
+++ routers/user.js
@@ -2,12 +2,13 @@ const Verify = require("../auth/verify");
 const express = require("express");
 const router = express.Router();
 
-router.get('/', Verify.verifyUser, function (req, res, next) {
-    res.status = 200;
-    res.json = {
+router.get('/', Verify.verifyUser, function(req, res, next) {
+    return res.status(200)
+    .json({
         status: "success",
         message: "You are loged in"
-    }
+    });
+
 });
 
 module.exports = router;
