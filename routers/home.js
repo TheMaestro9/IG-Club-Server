4dba12e4a3310382df74dc28b88ea1e5320e69c9
muha-abdulaziz <muha.abdulaziz@gmail.com>
2018-04-12 13:30:42 +0200

    Require validateSchema middleware and the postSchema into home router

--- routers/home.js
+++ routers/home.js
@@ -2,13 +2,15 @@ const express = require("express");
 const router = express.Router();
 const Verify = require("../auth/verify");
 
+let validateSchema = require('../json-schemaes/validateSchema')
+let postSchema = require("../json-schemaes/postSchema")
 
 const postController = require("../controllers/post");
 
 router.get('/posts', Verify.verifyUser, postController.getAllPosts);
-router.post('/posts', Verify.verifyUser, postController.createPost);
+router.post('/posts',validateSchema(postSchema), Verify.verifyUser, postController.createPost);
 
-router.put('/posts/:postId', Verify.verifyUser, postController.editPost);
+router.put('/posts/:postId', validateSchema(postSchema), Verify.verifyUser, postController.editPost);
 router.delete('/posts/:postId', Verify.verifyUser, postController.deletePost);
 
 module.exports = router
