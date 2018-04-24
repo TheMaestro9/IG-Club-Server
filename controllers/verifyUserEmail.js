df56ca9174590f46c1aa0da77ff16944911b3953
muha-abdulaziz <muha.abdulaziz@gmail.com>
2018-04-24 03:02:23 +0200

    Change the user verified statues in the data base if the verified link is correct

--- controllers/verifyUserEmail.js
+++ controllers/verifyUserEmail.js
@@ -1,12 +1,28 @@
 const jwt = require('jsonwebtoken')
+const models = require("../models");
+const User = models.User
+
 
 module.exports = function (req, res) {
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
     var decoded = jwt.decode(token)
     var userId = decoded.id
-    
-    return res.status(200).json({
-        success: true,
-        message: "You verified your mail."
+
+    User.findById(userId).then( user => {
+        return user.update({
+            verified: true
+        })
+        .then( _ => {
+            return res.status(200).json({
+                success: true,
+                message: "You verified your mail."
+            })
+        })
+        .catch( error => {
+            return res.status(500).json({
+                success: false,
+                message: "Error while verifing your mail."
+            })
+        })
     })
 }
