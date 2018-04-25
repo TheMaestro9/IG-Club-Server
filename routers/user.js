225533585fe3f5f1081eb789c6af77a0f1c0bd5d
TheMaestro9 <walidmoussa995@gmail.com>
2018-04-25 17:51:29 +0200

    adding check token and get user info functions

--- routers/user.js
+++ routers/user.js
@@ -1,7 +1,7 @@
 const Verify = require("../auth/verify");
 const express = require("express");
+const checkTokenController = require("../controllers/user")
 const router = express.Router();
-
 router.get('/', Verify.verifyUser, function(req, res, next) {
     return res.status(200)
     .json({
@@ -10,5 +10,9 @@ router.get('/', Verify.verifyUser, function(req, res, next) {
     });
 
 });
+
+router.get('/check-token', checkTokenController.checkToken);
+router.get('/user-info', checkTokenController.getUserInfo);
+
 
 module.exports = router;
