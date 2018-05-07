c00626ec98b1cbffe57427bb0759d1b6757b8dbc
TheMaestro9 <walidmoussa995@gmail.com>
2018-05-07 20:44:51 +0200

    removing schema validation from activities

--- routers/activetes.js
+++ routers/activetes.js
@@ -13,13 +13,13 @@ router.use(Verify.verifyUser);
 
 router.get("/", controllers.getActivities)
 // router.get("/interest", controllers.getInterest)
-router.post("/:activityId/interest", validateSchema(interestSchema), controllers.interestActivity)
+router.post("/:activityId/interest",  controllers.interestActivity)
 
 // Only Admis can add, edit or remove posts
 router.use(checkAdmin)
 
-router.post("/", validateSchema(schema), controllers.addActivity)
-router.put("/:activityId", validateSchema(schema), controllers.editActivity)
+router.post("/", controllers.addActivity)
+router.put("/:activityId", controllers.editActivity)
 router.delete("/:activityId", controllers.deleteActivity)
 
 module.exports = router
