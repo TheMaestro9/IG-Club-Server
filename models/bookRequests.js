35847e8c5045b77f37e20effa20c458df5d71cf0
TheMaestro9 <walidmoussa995@gmail.com>
2018-05-12 14:55:13 +0200

    removing unneeded models

--- models/bookRequests.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = (sequelize, DataTypes) => {
-    var BookRequests = sequelize.define('BookRequests', { 
-        UserId: DataTypes.INTEGER,
-        BookId: DataTypes.INTEGER
-    });
-    return BookRequests
-}
