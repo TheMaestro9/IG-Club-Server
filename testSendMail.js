1d3c110b46e241ced41922645d327d504280aba5
muha-abdulaziz <muha.abdulaziz@gmail.com>
2018-05-12 21:13:57 +0200

    refactor testSendMail and add functions to send mails for forgot password functions and use the new functions inside passport

--- testSendMail.js
+++ testSendMail.js
@@ -1,17 +1,50 @@
 const sendMail = require('./sendMail')
 const Verify = require('./auth/verify')
 
-module.exports = function (User) {
+var sendMsg = function (User, msg) {
+    let mail = msg 
+    
+    sendMail.SendAnEmail(mail.email, mail.subject, mail.content)
+}
+
+exports.forgotPass = function(User) {
     let payload = {
         id: User.id
     }
-    let token = Verify.getToken(payload)
-    let mail = {
+    let token = Verify.getShortToken(payload)
+    let msg = {
+        email: User.email,
+        subject: "Testing forgot password",
+        content: `go to this linke to verify your email\n
+            /user/verify/?token=${token}\n
+        this link will expire after 6 hours.`
+    }
+
+    sendMsg(User, msg)
+}
+
+exports.verifyMail = function (User) {
+    let payload = {
+        id: User.id
+    }
+    let token = Verify.getShortToken(payload)
+    let msg = {
         email: User.email,
         subject: "Testing verify email",
         content: `go to this linke to verify your email\n
-            /user/verify/?token=${token}`
+            /user/verify/?token=${token}\n
+        this link will expire after 6 hours.`
     }
-    
-    sendMail.SendAnEmail(mail.email, mail.subject, mail.content)
+
+    sendMsg(User, msg)
+}
+
+exports.newPassword = function (User, newPass) {
+    let msg = {
+        email: User.email,
+        subject: "Forgot Password",
+        content: ``
+    }
+
+    sendMsg(User, msg)
 }
