1f1841cfce1a5bed04e4542a87d5422f451cae35
TheMaestro9 <walidmoussa995@gmail.com>
2018-05-12 14:55:24 +0200

    adding seeder for courses

--- seeders/20180511111530-activitiesSeed.js
+++ seeders/20180511111530-activitiesSeed.js
@@ -2,14 +2,31 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('activities', [{
-    title: "Welcome Ya Shabab" ,
-    content:"You are very welcomed",
+    return queryInterface.bulkInsert('Activities', [{
+      title: "Paris Visit",
+      content: "You are very welcomed",
+      imageUrl: null,
+      type: "International Trips",
+      createdAt: new Date().toISOString().replace("T", " ").replace("Z", ""),
+      updatedAt: new Date().toISOString().replace("T", " ").replace("Z", "")
+    },
+    {
+      title: "Gmasa",
+      content: "1 day in gamasa",
+      imageUrl: null,
+      type: "Domestic Trips",
+      createdAt: new Date().toISOString().replace("T", " ").replace("Z", ""),
+      updatedAt: new Date().toISOString().replace("T", " ").replace("Z", "")
+
+    }, {
+    title: "Sharm El Shekh" ,
+    content:"4 days in sharm",
     imageUrl:null,
-    type:"International Trips",
+    type:"Domestic Trips",
     createdAt: new Date().toISOString().replace("T"," ").replace("Z","") , 
     updatedAt: new Date().toISOString().replace("T"," ").replace("Z","")
-}])
+    }
+    ])
   },
 
   down: (queryInterface, Sequelize) => {
---
+++ seeders/20180512123632-courses.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Courses', [{
+      description: "the great esl course",
+      name: "ESL",
+      currentPeriod: "2018-05-12 06:23:05",
+      nextRound: "2018-06-12 06:23:05",
+      cost: 500,
+      createdAt: new Date().toISOString().replace("T", " ").replace("Z", ""),
+      updatedAt: new Date().toISOString().replace("T", " ").replace("Z", "")
+    },
+    {
+      description: "apply for IELTS",
+      name: "IELTS",
+      currentPeriod: "2018-05-12 06:23:05",
+      nextRound: "2018-06-12 06:23:05",
+      cost: 1000,
+      createdAt: new Date().toISOString().replace("T", " ").replace("Z", ""),
+      updatedAt: new Date().toISOString().replace("T", " ").replace("Z", "")
+    }, {
+      description: "the great esl course",
+      name: "Admission",
+      currentPeriod: "2018-05-12 06:23:05",
+      nextRound: "2018-06-12 06:23:05",
+      cost: 100,
+      createdAt: new Date().toISOString().replace("T", " ").replace("Z", ""),
+      updatedAt: new Date().toISOString().replace("T", " ").replace("Z", "")
+    }, {
+      description: "Prepare yourself for IELTS",
+      name: "ILETS Preparation",
+      currentPeriod: "2018-05-12 06:23:05",
+      nextRound: "2018-06-12 06:23:05",
+      cost: 2000,
+      createdAt: new Date().toISOString().replace("T", " ").replace("Z", ""),
+      updatedAt: new Date().toISOString().replace("T", " ").replace("Z", "")
+    }])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
