b56196fd00b503d9fc74bf61d5a3d2aa7bb9715a
muha-abdulaziz <muha.abdulaziz@gmail.com>
2018-05-12 21:16:49 +0200

    Add delete user routers and its logic and add json schemas for email and password

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,9 @@
-const jwt = require("jsonwebtoken");
-const models = require("../models");
+const jwt = require("jsonwebtoken")
+const models = require("../models")
 const User = models.User
-
-
+const sendMail = require('../testSendMail')
+const tokenDecoded = require('./tokenDecoded')
+const passwdEncrypt = require('../config/passport/encrypt')
 
 
 exports.checkToken = (req, res) => {
@@ -45,4 +46,60 @@ exports.getUserInfo =(req,res)=>{
        })
    })
 
+}
+
+exports.forgotPassword = (req, res) => {
+    let email = req.body.email
+    User.findOne({
+        where: {
+            'email': email
+        }
+    })
+    .then(user => {
+        if (!user) {
+            return res.status(403)
+                .json({
+                    success: false,
+                    message: "email does not exist"
+                })
+        }
+        sendMail.forgotPass(user)
+        return res.status(200)
+        .json({
+            success: true,
+            message: "Check Your email"
+        })
+    })
+}
+
+exports.newPassword = (req, res) => {}
+
+exports.reVerifyMail = (req, res) => {}
+
+exports.updateUserInfo = (req, res) => {}
+
+exports.removeUser = (req, res) => {
+    let userId = tokenDecoded(req).id
+    let passwd = req.body.password
+
+    User.findById(userId)
+    .then(user => {
+        let validPass = passwdEncrypt.checkHash(passwd, user.password)
+        if (!validPass) {
+            return res.status(403)
+                .json({
+                    success: false,
+                    message: "Wrong password"
+                })
+        } else {
+            user.destroy({force: true})
+            .then(success => {
+                return res.status(200)
+                .json({
+                    success: true,
+                    message: "The user successfuly deleted."
+                })
+            })
+        }
+    })
 }
--- routers/user.js
+++ routers/user.js
@@ -1,22 +1,25 @@
 const Verify = require("../auth/verify");
-const verifyEmail = require('../controllers/verifyUserEmail')
 const express = require("express");
-const checkTokenController = require("../controllers/user")
+const verifyEmail = require('../controllers/verifyUserEmail')
+const userController = require("../controllers/user")
 const router = express.Router();
+
+let validateSchema = require('../json-schemaes/validateSchema')
+let emailSchema = require("../json-schemaes/emailSchema")
+let passwordSchema = require("../json-schemaes/passwordSchema")
+
+//router.post('/forgotPassword',validateSchema(emailSchema), userController.forgotPass)
+//router.post('/verify', validateSchema(emailSchema), userController.reVerifyMail)
 
 router.use('/', Verify.verifyUser)
 
-router.get('/', function(req, res, next) {
-    return res.status(200)
-    .json({
-        status: "success",
-        message: "You are loged in"
-    });
-
-});
-
-router.get('/user-info', checkTokenController.getUserInfo);
+router.get('/', userController.getUserInfo);
+// alice for '/' path
+router.get('/user-info', userController.getUserInfo);
+//router.put('/',validateSchema(passwordSchema), userController.updateUserInfo)
+router.delete('/',validateSchema(passwordSchema), userController.removeUser)
 
 router.get('/verify', verifyEmail)
+//router.get('/forgotPassword', userController.newPassword)
 
 module.exports = router;
